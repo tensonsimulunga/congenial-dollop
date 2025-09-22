6e4dc1972aa213045fd002ab3ff3a746fc47db5f
tensonsimulunga <tensonsimulunga@gmail.com>
2025-09-22 12:51:29 +0200

    my second assignment

---
+++ 2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int lenght,height,width,area;
+printf("enter lengt:");
+scanf("%d",&length);
+printf("enter width:");
+scanf("%d",width);
+printf("enter height");
+scanf("%d",height);
+area=height*width*length;
+printf("area of the rectangle=%d\n",area);
+return 0;
+}
+
