05520f1f2119cab487a3b818c85f3b63a36e0b0a
tensonsimulunga <tensonsimulunga@gmail.com>
2025-09-22 15:26:19 +0200

    assignment 3

---
+++ second.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+float radius,area;
+const float PI=3.14;
+printf("enter the radius of the circle:");
+scanf("%f",radius);
+area=PI*radius;
+printf("the area of the circle is:2%f\n",area);
+return 0;
+}
+
+
